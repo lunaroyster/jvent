1f19ac12c4e960cfc1f1eade2b1813f01e90d883
lunaroyster <oneinaquadrillion@live.com>
2016-12-02 17:27:06 +0000

    CRUDed event

--- api/v0/routes/event.js
+++ api/v0/routes/event.js
@@ -1,8 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
-router.use('/', );
-router.use('/:eventID', );
+router.post('/', );
+router.get('/', );
+
+router.get('/:eventID', );
+router.patch('/:eventID', );
+router.delete('/:eventID', );
+
 router.use('/:eventID/post', require('./post'));
 
 module.exports = router;
