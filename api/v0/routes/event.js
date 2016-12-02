f648ab8b91001c9ab5e5f5007db56a9818108df2
lunaroyster <oneinaquadrillion@live.com>
2016-12-02 17:16:32 +0000

    Event routes

--- api/v0/routes/event.js
+++ api/v0/routes/event.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
-
+router.use('/', );
+router.use('/:eventID', );
+router.use('/:eventID/post', require('./post'));
 
 module.exports = router;
--- api/v0/routes/index.js
+++ api/v0/routes/index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
 
-
+router.use('/event', require('./event'));
 
 module.exports = router;
