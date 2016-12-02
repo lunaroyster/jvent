dacefc8c44e5e3f21582775a8133c735ab1abc75
lunaroyster <oneinaquadrillion@live.com>
2016-12-02 17:18:02 +0000

    Comment routes

--- api/v0/routes/comment.js
+++ api/v0/routes/comment.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
-
+router.use('/', );
+router.use('/:commentID', );
+// router.use('/:postID/comment', require('./comment'));
 
 module.exports = router;
