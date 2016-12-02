83964bd3fa93766fa427aed3076a02f926cbae4d
lunaroyster <oneinaquadrillion@live.com>
2016-12-02 17:17:20 +0000

    Post routes

--- api/v0/routes/post.js
+++ api/v0/routes/post.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
-
+router.use('/', );
+router.use('/:postID', );
+router.use('/:postID/comment', require('./comment'));
 
 module.exports = router;
