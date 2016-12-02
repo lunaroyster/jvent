3e257bc06817705e69a5b25844ab25075817efc3
lunaroyster <oneinaquadrillion@live.com>
2016-12-02 17:42:44 +0000

    Linked comment to controller

--- api/v0/routes/comment.js
+++ api/v0/routes/comment.js
@@ -1,12 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
-router.post('/', );
-router.get('/', );
+var commentController = require('../controllers/comment');
 
-router.get('/:commentID', );
-router.patch('/:commentID', );
-router.delete('/:commentID', );
+router.post('/', commentController.createComment);
+router.get('/', commentController.getComments);
+
+router.get('/:commentID', commentController.getCommentByID);
+router.patch('/:commentID', commentController.updateCommentByID);
+router.delete('/:commentID', commentController.deleteCommentByID);
 // router.use('/:postID/comment', require('./comment'));
 
 module.exports = router;
