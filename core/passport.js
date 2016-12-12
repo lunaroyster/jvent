ba5e8e7ba919cc9880bcfc3e9f40838173a59041
lunaroyster <oneinaquadrillion@live.com>
2016-12-12 15:05:16 +0000

    JWT Strategy

--- core/passport.js
+++ core/passport.js
@@ -17,3 +17,16 @@ passport.use(new LocalStrategy({
     });
   }
 ));
+
+var JWTopts = {
+  jwtFromRequest: ExtractJwt.fromAuthHeader(),
+  secretOrKey: "test"
+}
+
+passport.use(new JwtStrategy(JWTopts, function(jwt_payload, done) {
+  User.findOne({_id: jwt_payload.sub}, function(err, user) {
+    if(err){return done(err, false)}
+    if(user){return done(null, user)}
+    else {return done(null, false)}
+  });
+}));
