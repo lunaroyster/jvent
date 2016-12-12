399e05e71b13ca2651a91ff4c3b4dd136d2e6e54
lunaroyster <oneinaquadrillion@live.com>
2016-12-12 15:14:59 +0000

    Passport intialization

--- app.js
+++ app.js
@@ -4,8 +4,10 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var passport = require('passport');
 
 require('./models/db');
+require('./core/passport');
 
 // var routes = require('./routes/index');
 // var users = require('./routes/users');
@@ -19,6 +21,7 @@ app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(passport.initialize());
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
