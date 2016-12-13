db45e2481c1c749ef9f9427bbb37e0e6bfb2ac35
lunaroyster <oneinaquadrillion@live.com>
2016-12-13 07:27:22 +0000

    Completed authentication function

--- api/v0/controllers/user.js
+++ api/v0/controllers/user.js
@@ -3,7 +3,7 @@ var userCore = require('../../../core/user');
 module.exports.authenticate = function(req, res) {
     userCore.generateToken(req.user, function(token) {
         res.status(200);
-        res.json(token);
+        res.json({token: token});
     });
 };
 
@@ -12,8 +12,8 @@ module.exports.signup = function(req, res) {
         email: req.body.email,
         username: req.body.username,
         password: req.body.password
-    };
+    }
     userCore.createUser(userObj, function(status) {
         res.json(status);
-    });
+    })
 };
--- models/user.js
+++ models/user.js
@@ -10,7 +10,21 @@ var userSchema = new Schema({
     username: String,
     hash: String,
     salt: String,
-    
+    password: String //Switch to hash/salt
 });
+//TODO: Encrypt
+userSchema.methods.setPassword = function(password, callback) {
+    this.password = password;
+    this.save(callback);
+};
+
+userSchema.methods.validPassword = function(password) {
+    if(password==this.password) {
+        return(true);
+    }
+    else {
+        return(false);
+    }
+};
 
 mongoose.model('User', userSchema);
