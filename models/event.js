a9524c95fae901baadd8a32ac5decc01265ef015
lunaroyster <oneinaquadrillion@live.com>
2017-01-10 06:57:19 +0000

    Required name, visibility, ingress in schema

--- models/event.js
+++ models/event.js
@@ -2,11 +2,20 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var eventSchema = new Schema({
-    name: String,
+    name: {
+        type: String,
+        required: true
+    },
     byline: String,
     description: String,
-    visibility: String,
-    ingress: String,
+    visibility: {
+        type: String,
+        required: true
+    },
+    ingress: {
+        type: String,
+        required: true
+    },
     posts: {
         type: [Schema.Types.ObjectId]
     },
