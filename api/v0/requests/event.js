f9e98a1986373e86870ce47ad9639ffc822e809a
lunaroyster <oneinaquadrillion@live.com>
2017-01-12 17:40:03 +0000

    Amended postEvent requestSchema

--- api/v0/requests/event.js
+++ api/v0/requests/event.js
@@ -4,11 +4,23 @@ module.exports.postEvent = {
         notEmpty: true,
         isLength: {
             options: [{ min: 4, max: 64 }],
-            errorMessage: 'Must be between 4 and 64 chars long'
-        },
+            errorMessage: 'Must be between 4 and 64 characters'
+        }
     },
     'event.byline': {
-        notEmpty: true
+        optional: true,
+        notEmpty: true,
+        isLength: {
+            options: [{max:128}],
+            errorMessage: 'Must be fewer than 128 characters'
+        }
+    },
+    'event.description': {
+        optional: true,
+        isLength: {
+            options: [{max:1024}],
+            errorMessage: 'Must be fewer than 1024 characters'
+        }
     },
     'event.visibility': {
         notEmpty: true
