af5bee8370bf836ac318b50b6ccbf2d2833140c1
lunaroyster <oneinaquadrillion@live.com>
2017-01-13 17:49:43 +0000

    Git createPost validator

--- api/v0/requests/post.js
+++ api/v0/requests/post.js
@@ -1,36 +1,17 @@
 module.exports.createPost = {
-    // 'event.name': {
-    //     notEmpty: true,
-    //     isLength: {
-    //         options: [{ min: 4, max: 64 }],
-    //         errorMessage: 'Must be between 4 and 64 characters'
-    //     }
-    // },
-    // 'event.byline': {
-    //     optional: true,
-    //     notEmpty: true,
-    //     isLength: {
-    //         options: [{max:128}],
-    //         errorMessage: 'Must be fewer than 128 characters'
-    //     }
-    // },
-    // 'event.description': {
-    //     optional: true,
-    //     isLength: {
-    //         options: [{max:1024}],
-    //         errorMessage: 'Must be fewer than 1024 characters'
-    //     }
-    // },
-    // 'event.visibility': {
-    //     notEmpty: true,
-    //     isVisibility: {
-    //         errorMessage: 'Invalid visibility setting'
-    //     }
-    // },
-    // 'event.ingress': {
-    //     notEmpty: true,
-    //     isIngress: {
-    //         errorMessage: 'Invalid ingress setting'
-    //     }
-    // }
+    'post.title': {
+        notEmpty: true,
+        isLength: {
+            options: [{max:256}],
+            errorMessage: 'The title can be at most 256 chars'
+        }
+    },
+    'post.content.text': {
+        optional: true,
+        notEmpty: true,
+        isLength: {
+            options: [{max:16384}],
+            errorMessage: 'The body can be at most 16384 chars'
+        }
+    }
 };
