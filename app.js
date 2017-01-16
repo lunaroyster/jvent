eda3aafb1b3903dc238a8218d49e2dda817a2555
lunaroyster <oneinaquadrillion@live.com>
2017-01-16 06:20:33 +0000

    Removed jade

--- app.js
+++ app.js
@@ -17,8 +17,8 @@ var apiRoute = require('./api/index');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
