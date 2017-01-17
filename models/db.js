6ff8dbf40679fc536a10bb95a5541f6a9f07735a
lunaroyster <oneinaquadrillion@live.com>
2017-01-17 11:44:45 +0000

    Q library for Mongoose promises

--- models/db.js
+++ models/db.js
@@ -18,6 +18,8 @@ mongoose.connection.on('disconnected', function() {
   console.log('Mongoose disconnected');
 });
 
+mongoose.Promise = require('q').Promise;
+
 // CAPTURE APP TERMINATION / RESTART EVENTS
 // To be called when process is restarted or terminated
 gracefulShutdown = function(msg, callback) {
