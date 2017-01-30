10848691798f68a1e743d5b0cf80a190f142fdf8
lunaroyster <oneinaquadrillion@live.com>
2017-01-30 17:33:31 +0000

    partialjwtAuth

--- api/v0/routes/user.js
+++ api/v0/routes/user.js
@@ -6,6 +6,16 @@ var userController = require('../controllers/user');
 var passport = require('passport');
 var jwtAuth = passport.authenticate('jwt', { session: false});
 
+var partialjwtAuth = function(req, res, next) {
+    passport.authenticate('jwt', {session:false}, function(err, user, info) {
+        if(user) {
+            req.user = user;
+        }
+        next();
+    })(req, res, next)
+}
+
+
 router.post('/authenticate', passport.authenticate('local', { session: false }), userController.authenticate);
 router.post('/signup', userController.signup);
 router.get('/', jwtAuth, userController.returnAuthenticatedUser);
