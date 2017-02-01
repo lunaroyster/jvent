000ab7e0eaba663359dbb1f2af33e65f59644246
lunaroyster <oneinaquadrillion@live.com>
2017-02-01 12:25:49 +0000

    Removed callback code (postController)

--- api/v0/controllers/post.js
+++ api/v0/controllers/post.js
@@ -4,27 +4,6 @@ var collectionCore = require('../../../core/collection');
 var postRequestSchema = require('../requests/post');
 
 // /post/
-// module.exports.createPost = function(req, res) {
-//     req.check(postRequestSchema.createPost);
-//     req.getValidationResult()
-//     .then(function(result) {
-//         if(!result.isEmpty()) {
-//             res.status(400);
-//             res.json(result.array());
-//             return;
-//         }
-//         //TODO: Cannot read property 'text' of undefined
-//         //TODO: Get rid of callback based code.
-//         var postSettings = {
-//             title: req.body.post.title,
-//             contentText: req.body.post.content.text
-//         };
-//         postCore.createPost(postSettings, req.eventID, function(state) {
-//             res.status(201);
-//             res.json(state);
-//         });
-//     });
-// };
 
 module.exports.createPost = function(req, res) {
     req.check(postRequestSchema.createPost);
@@ -90,14 +69,6 @@ module.exports.createPost = function(req, res) {
     });
 };
 
-// module.exports.getPosts = function(req, res) {
-//     postCore.getPosts(req.eventID, function(err, posts) {
-//         if(!err){
-//           res.json(posts); 
-//         }
-//     });
-// };
-
 module.exports.getPosts = function(req, res) {
     // get a promise
     // check req for querystring or parameters and format query
@@ -109,11 +80,6 @@ module.exports.getPosts = function(req, res) {
 };
 
 // /post/:postID
-// module.exports.getPostByID = function(req, res) {
-//     postCore.getPostByID(req.eventID, req.params.postID, function(err, post) {
-//         res.json(post);
-//     });
-// };
 
 module.exports.getPostByID = function(req, res) {
     // Check user privilege
