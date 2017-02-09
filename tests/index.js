05593bce3adba8cee3393541e71bd7d03ec97d6b
lunaroyster <oneinaquadrillion@live.com>
2017-02-09 15:57:48 +0000

    Drops database every test

--- tests/index.js
+++ tests/index.js
@@ -1,7 +1,13 @@
+var mongoose = require('mongoose');
 var data = require("./data");
 var app = data.app;
 
 describe("tests", function () {
+    before(function() {
+       mongoose.connection.db.dropDatabase(function(err, result) {
+           if(err) throw err;
+       });
+    });
     beforeEach(function () {
         
     });
