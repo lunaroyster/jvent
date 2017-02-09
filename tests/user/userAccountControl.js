be49907f32c21db85f9733a1c68c43a5e6dedf76
lunaroyster <oneinaquadrillion@live.com>
2017-02-09 15:46:16 +0000

    Wrote first test

--- tests/user/userAccountControl.js
+++ tests/user/userAccountControl.js
@@ -1,13 +1,18 @@
 var supertest = require('supertest');
 var data = require("../data");
 var app = data.app;
+// var app = require('../../app.js')
+var agent = supertest.agent(app);
 
 describe("user account control", function() {
-    it("creates a user", function() {
-        supertest(app)
+    it("creates a user", function(done) {
+        agent
         .post('/api/v0/user/signup')
         .send(data.user)
-        .expect(201);
+        .expect(201)
+        .end(function(err, res) {
+            done(err)
+        });
     });
     it("doesn't allow incomplete information")
     it("doesn't allow duplicate users")
