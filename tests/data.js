e8d60479bfebd29593805f68f2bdbeb52e3fbda4
lunaroyster <oneinaquadrillion@live.com>
2017-02-09 16:16:16 +0000

    More tests

--- tests/data.js
+++ tests/data.js
@@ -4,5 +4,20 @@ module.exports.user = {
     password: "t35tl012D"
 };
 
+module.exports.users = {
+    noUsername: {
+        email: "test@unpaidinterns.example.org",
+        password: "t35tl012D"
+    },
+    noEmail: {
+        username: "test",
+        password: "t35tl012D"
+    },
+    noPassword: {
+        username: "test",
+        email: "test@unpaidinterns.example.org"
+    }
+}
+
 // Reduces the number of dependent links that would break if app.js had to be moved.
 module.exports.app = require("../app.js");
--- tests/user/userAccountControl.js
+++ tests/user/userAccountControl.js
@@ -11,10 +11,38 @@ describe("user account control", function() {
         .send(data.user)
         .expect(201)
         .end(function(err, res) {
-            done(err)
+            done(err);
         });
     });
-    it("doesn't allow incomplete information")
+    describe("doesn't create user with incomplete information", function() {
+        it("doesn't create user without username", function(done) {
+            agent
+            .post('/api/v0/user/signup')
+            .send(data.users.noUsername)
+            .expect(400)
+            .end(function(err, res) {
+                done(err);
+            });
+        });
+        it("doesn't create user without password", function(done) {
+            agent
+            .post('/api/v0/user/signup')
+            .send(data.users.noPassword)
+            .expect(400)
+            .end(function(err, res) {
+                done(err);
+            });
+        });
+        it("doesn't create user without email", function(done) {
+            agent
+            .post('/api/v0/user/signup')
+            .send(data.users.noEmail)
+            .expect(400)
+            .end(function(err, res) {
+                done(err);
+            });
+        })
+    })
     it("doesn't allow duplicate users")
     it("generates JWT for user")
     it("tests JWT authentication")
