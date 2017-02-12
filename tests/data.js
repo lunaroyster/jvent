d25b75bd89c830ef6f42656b091fc6798d30e852
lunaroyster <oneinaquadrillion@live.com>
2017-02-12 17:44:51 +0000

    Test events

--- tests/data.js
+++ tests/data.js
@@ -28,5 +28,44 @@ module.exports.users = {
     }
 };
 
+module.exports.events = {
+    festival: {
+        name: "Christmas",
+        byline: "Christmas 2017",
+        description: "God bless consumerism!",
+    },
+    protest: {
+        name: "#WantChange protests",
+        byline: "Change! Slogans! Posters!",
+        description: "And riots!"
+    },
+    blockParty: {
+        name: "PARDY mah house",
+        byline: "i got stuffs",
+        description: "bleh"
+    },
+    publicConvention: {
+        name: "GeneriCON",
+        byline: "New Stuff! Cosplay! Food!",
+        description: "Awkward panels"
+    },
+    schoolHackathon: {
+        name: "Hack 2017",
+        byline: "Code! Geeks! Pizza! Prizes!",
+        description: "Unfinished projects! Awkward presentations!"
+    },
+    prom: {
+        name: "Generic HS Prom Night",
+        byline: "Dance! Music! Romance!",
+        description: "Awkward Dancing! Lame Music! STDs!"
+    },
+    bookSigning: {},
+    generic: {
+        name: "Generic Event",
+        byline: "Nothing interesting",
+        description: "No BS"
+    }
+};
+
 // Reduces the number of dependent links that would break if app.js had to be moved.
 module.exports.app = require("../app.js");
