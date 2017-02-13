46cc48fb852486d99aed35efed6c2c55fc3fd1f2
lunaroyster <oneinaquadrillion@live.com>
2017-02-13 09:30:53 +0000

    Added settings to data.events

--- tests/data.js
+++ tests/data.js
@@ -33,37 +33,58 @@ module.exports.events = {
         name: "Christmas",
         byline: "Christmas 2017",
         description: "God bless consumerism!",
+        visibility: "public",
+        ingress: "everyone"
     },
     protest: {
         name: "#WantChange protests",
         byline: "Change! Slogans! Posters!",
-        description: "And riots!"
+        description: "And riots!",
+        visibility: "public",
+        ingress: "link"
     },
     blockParty: {
         name: "PARDY mah house",
         byline: "i got stuffs",
-        description: "bleh"
+        description: "bleh",
+        visibility: "unlisted",
+        ingress: "link"
     },
-    publicConvention: {
+    fanConvention: {
         name: "GeneriCON",
         byline: "New Stuff! Cosplay! Food!",
-        description: "Awkward panels"
+        description: "Awkward panels",
+        visibility: "public",
+        ingress: "link"
+    },
+    concert: {
+        name: "Heavy stuff",
+        byline: "Headbanging! Drinking!",
+        description: "Whiplash! Hearing loss!",
+        visibility: "Public",
+        ingress: "invite"
     },
     schoolHackathon: {
         name: "Hack 2017",
         byline: "Code! Geeks! Pizza! Prizes!",
-        description: "Unfinished projects! Awkward presentations!"
+        description: "Unfinished projects! Awkward presentations!",
+        visibility: "unlisted",
+        ingress: "invited"
     },
     prom: {
         name: "Generic HS Prom Night",
         byline: "Dance! Music! Romance!",
-        description: "Awkward Dancing! Lame Music! STDs!"
+        description: "Awkward Dancing! Lame Music! STDs!",
+        visibility: "private",
+        ingress: "invite"
     },
     bookSigning: {},
     generic: {
         name: "Generic Event",
         byline: "Nothing interesting",
-        description: "No BS"
+        description: "No BS",
+        visibility: "public",
+        ingress: "everyone"
     }
 };
 
