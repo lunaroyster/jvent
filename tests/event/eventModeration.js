5c0787bc9e2bef46554c60d58f269f9105e65620
lunaroyster <oneinaquadrillion@live.com>
2017-02-14 15:57:47 +0000

    eventModeration support functions

--- tests/event/eventModeration.js
+++ tests/event/eventModeration.js
@@ -1,3 +1,41 @@
+var supertest = require('supertest');
+var async = require('async');
+var Q = require('q');
+var data = require("../data");
+var app = data.app;
+// var app = require('../../app.js')
+var agent = supertest.agent(app);
+
+var JWT = "";
+
+var signupUser = function(user) {
+    var deferred = Q.defer();
+    agent
+    .post('/api/v0/user/signup')
+    .send(user)
+    .expect(201)
+    .end(function(err, res) {
+        if(err) return deferred.reject(new Error(err));
+        return deferred.resolve(res.user);
+    });
+    return deferred.promise;
+};
+
+var createEvent = function(event) {
+    var deferred = Q.defer();
+    agent
+    .post('/api/v0/event')
+    .set('Authorization', 'JWT ' + JWT)
+    .set('Content-Type', 'application/json')
+    .send({event:event})
+    .expect(201)
+    .end(function(err, res) {
+        if(err) return deferred.reject(new Error(err));
+        return deferred.resolve(res.body.event);
+    });
+    return deferred.promise; 
+}
+
 describe("event moderation", function() {
     
 });
