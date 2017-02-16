c28ed4bf7c4cdc7dfe32226cea975bf37b48846c
lunaroyster <oneinaquadrillion@live.com>
2017-02-16 11:48:01 +0000

    Post Creation outline

---
+++ tests/post/postCreation.js
@@ -0,0 +1,24 @@
+describe("post creation", function() {
+    before(function() {
+        
+    });
+    describe("types of posts", function() {
+        it("creates title posts", function() {
+            
+        });
+        it("creates text posts", function() {
+            
+        });
+        it("creates link posts", function() {
+            
+        });
+    });
+    describe("broken posts", function() {
+        it("doesn't create without title", function() {
+            
+        });
+    });
+    describe("verifying created posts", function() {
+        
+    });
+})
