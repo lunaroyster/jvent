a7b37554042cc1cc402c2e5dc212e7cf55e37fd1
lunaroyster <oneinaquadrillion@live.com>
2017-02-23 11:41:27 +0000

    path.js for models

---
+++ models/path.js
@@ -0,0 +1,8 @@
+module.exports = {
+    event: require('./event'),
+    post: require('./post'),
+    collection: require('./collection'),
+    superCollection: require('./supercollection'),
+    user: require('./user'),
+    userList: require('./userlist')
+};
--- path.js
+++ path.js
@@ -1,17 +1,6 @@
 module.exports = {
-    core: {
-        collection: require("./core/collection"),
-        event: require("./core/event"),
-        passport: require("./core/passport"),
-        post: require("./core/post"),
-        requestOptions: require("./core/requestOptions"),
-        url: require("./core/url"),
-        user: require("./core/user"),
-        userList: require("./core/userList"),
-    },
-    models: {
-        
-    },
+    core: require("./core/path"),
+    models: require("./models/path"),
     api: {
         
     },
