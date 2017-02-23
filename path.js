348beb8b724769a1daaa62fadb957a54d2238d40
lunaroyster <oneinaquadrillion@live.com>
2017-02-23 10:31:02 +0000

    path.js

---
+++ path.js
@@ -0,0 +1,19 @@
+module.exports = {
+    core: {
+        collection: require("./core/collection"),
+        event: require("./core/event"),
+        passport: require("./core/passport"),
+        post: require("./core/post"),
+        requestOptions: require("./core/requestOptions"),
+        url: require("./core/url"),
+        user: require("./core/user"),
+        userList: require("./core/userList"),
+    },
+    models: {
+        
+    },
+    api: {
+        
+    },
+    
+};
