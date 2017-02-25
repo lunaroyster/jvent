39d168babc0232bb41d47e081ba5aba5ede27ab6
lunaroyster <oneinaquadrillion@live.com>
2017-02-25 18:21:18 +0000

    /users/ GET functions

--- api/v0/routes/event.js
+++ api/v0/routes/event.js
@@ -4,15 +4,27 @@ var router = express.Router();
 var eventController = require('../controllers/event');
 var authController = require('../controllers/auth');
 
+// /event/
 router.post('/', authController.AuthOnly, eventController.createEvent);
 router.get('/', eventController.getEvents);
 
+// /event/:eventURL
 //TODO: Remove appendEvent from these paths. Query directly at the router (only for required fields)
 router.get('/:eventURL', eventController.appendEventIfVisible, eventController.getEvent);
 router.patch('/:eventURL', authController.AuthOnly, eventController.appendEventIfVisible, eventController.updateEvent);
 router.delete('/:eventURL', authController.AuthOnly, eventController.appendEventIfVisible, eventController.deleteEvent);
 
+// /event/:eventURL/[function]
+//TODO: Route separately?
+router.get('/:eventURL/users');
+router.get('/:eventURL/users/attending');
+router.get('/:eventURL/users/viewing');
+router.get('/:eventURL/users/moderating');
+router.get('/:eventURL/users/invited');
+
 router.patch('/:eventURL/join', authController.AuthOnly, eventController.appendEventIfVisible, eventController.joinEvent);
+
+// /event/:eventURL/post
 router.use('/:eventURL/post', eventController.appendEventIfVisible, require('./post'));
 
 module.exports = router;
