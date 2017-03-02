2dbe173d4ecd3fd4a80cdbdbbf895ce30a695cd1
lunaroyster <oneinaquadrillion@live.com>
2017-03-02 10:39:46 +0000

    Registered eventMembership

--- models/eventmembership.js
+++ models/eventmembership.js
@@ -2,8 +2,8 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var eventMembershipSchema = new Schema({
-    user: {},
-    event: {},
+    user: { type: Schema.Types.ObjectId },
+    event: { type: Schema.Types.ObjectId },
     role: String
 });
 
