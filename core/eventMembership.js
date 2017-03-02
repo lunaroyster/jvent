a1d9eb20d4cd4db209f38677dc52956f896398e8
lunaroyster <oneinaquadrillion@live.com>
2017-03-02 17:51:47 +0530

    eventMembership Core

---
+++ core/eventMembership.js
@@ -0,0 +1,19 @@
+var mongoose = require('mongoose');
+var Q = require('q');
+var UserList = mongoose.model('eventMembership');
+
+module.exports.isUserAttendee = function(user, event) {
+
+};
+
+module.exports.isUserViewer = function(user, event) {
+
+};
+
+module.exports.isUserInvitee = function(user, event) {
+
+};
+
+module.exports.isUserModerator = function(user, event) {
+
+};
