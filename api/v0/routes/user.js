9ada94b82cd84e0b2a75d5d427c5ab39f5f448bd
lunaroyster <oneinaquadrillion@live.com>
2017-03-05 11:35:24 +0000

    /user controller changes

--- api/v0/routes/user.js
+++ api/v0/routes/user.js
@@ -5,10 +5,18 @@ var userController = require('../controllers/user');
 var authController = require('../controllers/auth');
 var AuthOnly = authController.AuthOnly;
 
+//TODO: Restructure authentication and password routes
 router.post('/changepassword', AuthOnly, userController.changePassword);
 router.post('/authenticate', authController.localAuth, userController.authenticate);
 router.post('/signup', userController.signup);
 router.get('/', AuthOnly, userController.returnAuthenticatedUser);
 router.get('/me', AuthOnly, userController.returnAuthenticatedUser);
+router.use('/events', AuthOnly, eventsRouter);
+
+var eventsRouter = express.Router();
+eventsRouter.get('/viewing', userController.getVisibleEvents);
+eventsRouter.get('/attending', userController.getAttendedEvents);
+eventsRouter.get('/invited', userController.getInvitedEvents);
+eventsRouter.get('/moderating', userController.getModeratedEvents);
 
 module.exports = router;
