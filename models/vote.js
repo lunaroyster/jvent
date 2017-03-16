fea4291ead096b08e1d2675824572a59fd486b57
lunaroyster <oneinaquadrillion@live.com>
2017-03-16 16:35:52 +0000

    voteSchema vote methods

--- models/vote.js
+++ models/vote.js
@@ -14,4 +14,25 @@ var voteSchema = new Schema({
     time: Date
 });
 
+voteSchema.methods.switchDirection = function(d) {
+    if(this.direction!=d) {
+        this.direction = d;
+        this.time = Date.now();
+        return true;
+    }
+    return false;
+};
+
+voteSchema.methods.upvote = function() {
+    return this.switchDirection(1);
+};
+
+voteSchema.methods.downvote = function() {
+    return this.switchDirection(-1);
+};
+
+voteSchema.methods.unvote = function() {
+    return this.switchDirection(0);
+};
+
 mongoose.model('Vote', voteSchema);
