b8f64a32c896b1a10af4145176a814534d6c4ecc
lunaroyster <oneinaquadrillion@live.com>
2017-03-21 18:12:02 +0000

    eventFindQuery structure

--- core/eventFindQuery.js
+++ core/eventFindQuery.js
@@ -5,9 +5,21 @@ var Event = mongoose.model('Event');
 
 var eventFindQuery = function() {
     this.Event = Event;
+    this.sort = {};
+    this.find = {};
+    this.field = {};
+    this.limit = {};
 };
+
 eventFindQuery.prototype = {
-    
+    //find
+    //sort
+    //field
+    //limit
+    //other
+    then: function() {
+        //Convert to query and return promise
+    }
 };
 
 module.exports.eventFindQuery = eventFindQuery;
