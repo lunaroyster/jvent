e44563664e9ef0c2dd242b2827756aa689eb8d50
lunaroyster <oneinaquadrillion@live.com>
2017-03-22 13:25:26 +0000

    eventFindQuery.prototype pseudocode

--- core/eventFindQuery.js
+++ core/eventFindQuery.js
@@ -5,34 +5,45 @@ var Event = mongoose.model('Event');
 
 var eventFindQuery = function() {
     this.Event = Event;
-    this.sort = {};
     this.find = {};
-    this.field = {};
+    this.sort = {};
     this.limit = {};
+    this.field = {};
 };
 
 eventFindQuery.prototype = {
     //find
     all: function() {
+        //reset finds
         return this;
     },
     location: function(location) {
+        //verify location is legitimate
+        //enable location search and store provided loc
         return this;
     },
     time: function(start, end) {
+        //verify args are legitimate time values
+        //enable time search and store provided time
         return this;
     },
     organizer: function(organizer) {
+        //enable org search and store provided organizer
         return this;
     },
     genre: function(genre) {
+        //verify genre exists and store provided genre
         return this;
     },
     //sort
     byTime: function(direction) {
+        //verify direction
+        //Enable time sort and store provided direction
         return this;
     },
-    byRank: function() {
+    byRank: function(rankType) {
+        //Verify rankType is real
+        //Enable rank sort and store provided rankType
         return this;
     },
     //field
@@ -45,14 +56,19 @@ eventFindQuery.prototype = {
         this.field.fields.push(Array.from(arguments));
         return this;
     },
+    noSelect: function() {
+        //TODO: complete
+        this.field.fields = [];
+        return this;
+    },
     //limit
     limit: function(n) {
-        //check if n is acceptable
+        //check if n is valid
         this.limit.count = n;
         return this;
     },
     page: function(n) {
-        //check if acceptable.
+        //check if n is valid.
         //Switch to offsets?
         this.limit.page = n;
         return this;
