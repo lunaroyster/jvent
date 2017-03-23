4075ee9e4cbaa07e79aca1f1014b4ab74bca5260
lunaroyster <oneinaquadrillion@live.com>
2017-03-23 13:37:55 +0000

    postFindQuery

---
+++ core/postFindQuery.js
@@ -0,0 +1,24 @@
+var mongoose = require('mongoose');
+var Q = require('q');
+var _ = require('underscore')._;
+var assert = require('chai').assert;
+
+var Event = mongoose.model('Event');
+var Post = mongoose.model('Post');
+
+var postFindQuery = function(event) {
+    
+};
+
+postFindQuery.prototype = {
+    //find
+    //sort
+    //field
+    //limit
+    //other
+    then: function() {
+        
+    }
+};
+
+module.exports = postFindQuery;
