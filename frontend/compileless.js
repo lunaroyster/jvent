54f36afefa026c42a4b14584820e2a1b85977978
lunaroyster <oneinaquadrillion@live.com>
2017-03-31 17:31:35 +0000

    Fixed compileless

--- frontend/compileless.js
+++ frontend/compileless.js
@@ -3,6 +3,10 @@ var path = require('path');
 var less = require('less');
 var Q = require('q');
 
+function toCSS(lessFileName) {
+    return lessFileName.replace(".less", ".css");
+}
+
 var render = function() {
     var lessFolder = "./less/";
     var outFolder = "./public/less/";
@@ -11,16 +15,23 @@ var render = function() {
     })
     .then(function(lessFiles) {
         var filePromises = [];
-        for (var lessFile in lessFiles) {
+        for (var lF in lessFiles) {
+            var lessFile = lessFiles[lF];
             var filePromise = less.render(fs.readFileSync(lessFolder + lessFile).toString())
             .then(function(output) {
-                fs.writeFile(outFolder+lessFile, output.css, {flag:'wx'}, function(error, output) {
+                var deferred = Q.defer();
+                fs.writeFile(outFolder+toCSS(lessFile), output.css, {flag:'w'}, function(error, output) {
+                    deferred.resolve();
                     return;
                 });
+                return deferred.promise;
             });
             filePromises.push(filePromise);
         }
         return Q.all(filePromises);
+    })
+    .then(function() {
+        console.log("Compiled LESS files");
     });
 };
 render();
