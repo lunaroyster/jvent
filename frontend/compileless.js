69507eb2b7570b5664563993aae369a199480ea7
lunaroyster <oneinaquadrillion@live.com>
2017-03-31 16:41:21 +0000

    less compiler

--- frontend/compileless.js
+++ frontend/compileless.js
@@ -1,13 +1,26 @@
 var fs   = require('fs');
 var path = require('path');
 var less = require('less');
+var Q = require('q');
 
 var render = function() {
-    var src = "./less/style.less";
-    less.render(fs.readFileSync(src).toString(), function(e, output) {
-        fs.writeFile('./compiledless/test.css', output.css, {flag:'wx'}, function(error, output) {
-            
-        });
+    var lessFolder = "./less/";
+    var outFolder = "./public/less/";
+    Q.fcall(function() {
+        return fs.readdirSync(lessFolder);
+    })
+    .then(function(lessFiles) {
+        var filePromises = [];
+        for (var lessFile in lessFiles) {
+            var filePromise = less.render(fs.readFileSync(lessFolder + lessFile).toString())
+            .then(function(output) {
+                fs.writeFile(outFolder+lessFile, output.css, {flag:'wx'}, function(error, output) {
+                    return;
+                });
+            });
+            filePromises.push(filePromise);
+        }
+        return Q.all(filePromises);
     });
 };
 render();
