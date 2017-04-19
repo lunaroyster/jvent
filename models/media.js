35035fba04eb7a32790241ae023050d8088cef06
lunaroyster <oneinaquadrillion@live.com>
2017-04-19 10:56:06 +0000

    Temporary mediaDelegate fields

--- models/media.js
+++ models/media.js
@@ -3,15 +3,29 @@ var Schema = mongoose.Schema;
 
 var mediaSchema = new Schema({
     timeOfCreation: Date,
+    time: {
+        creation: {
+            type: Date
+        }
+    },
+    // For Later {
     type: String, //image/video/gif/link
     strategy: String, //jvent/imgur/other
     access: {
         type: Schema.Types.Mixed //Whatever required to access the resource 
     },
+    // }
+    // Temporary implementation {
+    link: String,    
+    // }
     event: {
         type: Schema.Types.ObjectId,
         ref: 'Event'
     },
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    }
 });
 
 mongoose.model('Media', mediaSchema);
