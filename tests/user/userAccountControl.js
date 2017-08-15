5096471b3a1d8dd52815f065aa6fec6a8485d557
lunaroyster <oneinaquadrillion@live.com>
2017-08-15 17:17:33 +0000

    userAccountControl tests

--- tests/user/userAccountControl.js
+++ tests/user/userAccountControl.js
@@ -51,32 +51,29 @@ var failAuthenticate = function(email, password) {
     return authenticate(email, password, 400);
 };
 
-
 describe("user account control", function() {
-    describe("account creation/deletion", function() {
-        it("creates user", function(done) {
-            agent
-            .post('/api/v0/user/signup')
-            .send({user: data.users.test})
-            .expect(201)
-            .end(function(err, res) {
-                done(err);
-            });
-        });
-        describe("doesn't create user with incomplete information", function() {
-            it("doesn't create user without username", function(done) {
+    describe("account creation", function() {
+        describe("user creation with valid information", function() {
+            it("creates user", function(done) {
                 agent
                 .post('/api/v0/user/signup')
-                .send({user: data.users.noUsername})
-                .expect(400)
+                .send({user: data.users.test})
+                .expect(201)
                 .end(function(err, res) {
                     done(err);
                 });
             });
-            it("doesn't create user without password", function(done) {
+        });
+        describe("user non-creation with invalid information", function() {
+            it("doesn't create user with short username");
+            it("doesn't create user with bad email");
+            it("doesn't create user with easy passwords");
+        });
+        describe("user non-creation with incomplete information", function() {
+            it("doesn't create user without username", function(done) {
                 agent
                 .post('/api/v0/user/signup')
-                .send({user: data.users.noPassword})
+                .send({user: data.users.noUsername})
                 .expect(400)
                 .end(function(err, res) {
                     done(err);
@@ -91,78 +88,84 @@ describe("user account control", function() {
                     done(err);
                 });
             });
-        });
-        describe("doesn't create user with duplicate information", function() {
-            it("doesn't allow duplicate users", function(done) {
-                //TODO: Write new user. reduce redundant code
-                async.series([
-                    function(cb) {
-                        agent
-                        .post('/api/v0/user/signup')
-                        .send({user: data.users.jeff})
-                        .expect(201)
-                        .end(function(err, res) {
-                            cb(err);
-                        });
-                    },
-                    function(cb) {
-                        agent
-                        .post('/api/v0/user/signup')
-                        .send({user: data.users.jeff})
-                        .expect(400)
-                        .end(function(err, res) {
-                            done(err);
-                        });
-                    }
-                ], done);
-            });
-        });
-        it("deletes user");
-    });
-    describe("authentication", function() {
-        it("generates JWT for user", function(done) {
-            agent
-            .post('/api/v0/user/authenticate')
-            .type('form')
-            .set('Content-Type', 'application/x-www-form-urlencoded')
-            .send({'email':data.users.test.email, 'password':data.users.test.password})
-            .expect(200)
-            .end(function(err, res) {
-                if (err) throw err;
-                jwt.verify(res.body.token, "debug", function(err, decoded) {
-                    if (err) throw err;
-                    JWT = res.body.token;
-                    done();
-                    return;
+            it("doesn't create user without password", function(done) {
+                agent
+                .post('/api/v0/user/signup')
+                .send({user: data.users.noPassword})
+                .expect(400)
+                .end(function(err, res) {
+                    done(err);
                 });
             });
         });
-        it("tests JWT authentication", function(done) {
-            agent
-            .get('/api/v0/user/me')
-            .set('Authorization', 'JWT ' + JWT)
-            .expect(200)
-            .end(function(err, res) {
-                done(err);
+        describe("user non-creation with duplicate information", function() {
+            it("doesn't create user with existing email");
+            it("doesn't create user with existing username");
+        });
+    });
+    describe("account deletion", function() {
+        it("deletes user with authentication");
+        it("doesn't delete user without authentication");
+    });
+    describe("authentication", function() {
+        describe("JSON Web Token based", function() {
+            describe("JWT generation", function() {
+                it("generates JWT for user", function(done) {
+                    agent
+                    .post('/api/v0/user/authenticate')
+                    .type('form')
+                    .set('Content-Type', 'application/x-www-form-urlencoded')
+                    .send({'email':data.users.test.email, 'password':data.users.test.password})
+                    .expect(200)
+                    .end(function(err, res) {
+                        if (err) throw err;
+                        jwt.verify(res.body.token, "debug", function(err, decoded) {
+                            if (err) throw err;
+                            JWT = res.body.token;
+                            done();
+                            return;
+                        });
+                    });
+                });
+            });
+            describe("JWT authentication test", function() {
+                it("tests JWT authentication", function(done) {
+                    agent
+                    .get('/api/v0/user/me')
+                    .set('Authorization', 'JWT ' + JWT)
+                    .expect(200)
+                    .end(function(err, res) {
+                        done(err);
+                    });
+                });
             });
         });
     });
-    describe("password changes", function() {
-        it("changes password", function() {
-            return successfulChangePassword(JWT, data.users.test.password, `${data.users.test.password}changed`)
-            .then(function() {
-                return successfulAuthenticate(data.users.test.email, data.users.test.password);
+    describe("password change", function() {
+        describe("successful password change", function() {
+            it("changes password", function() {
+                return successfulChangePassword(JWT, data.users.test.password, `${data.users.test.password}changed`)
+                .then(function() {
+                    return successfulAuthenticate(data.users.test.email, data.users.test.password);
+                });
             });
         });
-        it("doesn't change password with wrong creds", function() {
-            return failChangePassword(JWT, data.users.test.password, `${data.users.test.password}changed`)
-            .then(function() {
-                return failAuthenticate(data.users.test.email, `${data.users.test.password}changed`);
+        describe("failed password change", function() {
+            describe("due to bad credentials", function() {
+                it("doesn't change password with wrong creds", function() {
+                    return failChangePassword(JWT, data.users.test.password, `${data.users.test.password}changed`)
+                    .then(function() {
+                        return failAuthenticate(data.users.test.email, `${data.users.test.password}changed`);
+                    });
+                }); //Obsolete
+                it("doesn't change password without JWT");
+                it("doesn't change password without old password");
+                it("doesn't change password with incorrect old password");
             });
-        });
-        describe("terrible passwords", function() {
-            it("fails short passwords");
-            it("fails super long passwords");
+            describe("due to bad password", function() {
+                it("fails short passwords");
+                it("fails super long passwords");
+            });
         });
     });
-})
+});
