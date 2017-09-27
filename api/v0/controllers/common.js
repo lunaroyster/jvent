93c61dae971a67d9a9350039473e7e833cb59a1c
lunaroyster <oneinaquadrillion@live.com>
2017-09-27 16:49:58 +0000

    route fixes; migrated event.js to use async/await

--- api/v0/controllers/common.js
+++ api/v0/controllers/common.js
@@ -140,3 +140,7 @@ module.exports.appendEventIfVisible = function(req, res, next) {
         next(error);
     });
 };
+
+module.exports.asyncWrap = fn => (req, res, next) => {
+    Promise.resolve(fn(req, res, next)).catch(next);
+};
--- api/v0/controllers/event.js
+++ api/v0/controllers/event.js
@@ -7,6 +7,7 @@ const assert = require('chai').assert;
 const common = require('./common');
 const validateRequest = common.validateRequest;
 const packError = common.packError;
+const asyncWrap = common.asyncWrap;
 const createMediaTemplateFromRequest = common.createMediaTemplateFromRequest;
 const EventMembership = eventMembershipCore.EventMembership;
 
@@ -17,8 +18,7 @@ badAuthError.status = 404;
 // /event/
 var checkCreateEventPrivilege = function(req) {
     if(!req.user.privileges.createEvent) throw new Error("Bad privileges");
-    return;
-}
+};
 var createEventTemplateFromRequest = function(req, event) {
     return {
         name: event.name,
@@ -29,187 +29,147 @@ var createEventTemplateFromRequest = function(req, event) {
         comment: event.comment,
         user: req.user
     };
-}
-module.exports.createEvent = function(req, res) {
-    Q.fcall(()=> {
-        return validateRequest(req, eventRequestSchema.createEvent)
-    })
-    .then(()=> {
-        return checkCreateEventPrivilege(req);
-    })         //Check user privileges
-    .then(()=> {
-        var eventTemplate = createEventTemplateFromRequest(req, req.body.event);
-        // var mediaTemplate = undefined;
-        // if(req.body.media) {
-        //     mediaTemplate = createMediaTemplateFromRequest(req, req.body.media)
-        // }
-        return eventCore.createEvent(eventTemplate);
-    })         //Create event (using authenticated user)
-    .then((event)=> {
-        var state = {
+};
+var createEvent = async function(req, res) {
+    try {
+        await validateRequest(req, eventRequestSchema.createEvent);
+        await checkCreateEventPrivilege(req);
+        let eventTemplate = createEventTemplateFromRequest(req, req.body.event);
+        let event = await eventCore.createEvent(eventTemplate);
+        let state = {
             status: "Created",
-            event: {
-                url: event.url,
-                joinUrl: event.joinUrl
-            }
+            event: {url: event.url, joinUrl: event.joinUrl}
         };
         res.status(201).json(state);
-        return;
-    })    //Send event creation success
-    .catch((error)=> {
-        var err = packError(error);
+    }
+    catch (error) {
+        let err = packError(error);
         res.status(400).json(err);
-    });
+    }
 };
 
-module.exports.getEvents = function(req, res) {
-    var responseObject = {};
-    eventCore.getPublicEvents()
-    .then((events)=> {
-        responseObject.events = events;
-        res.status(200);
-        res.json(responseObject);
-    }, (error)=> {
-        responseObject.error = error;
-        res.status(400);
-        res.json(responseObject);
-    });
+var getEvents = async function(req, res) {
+    try {
+        let publicEvents = await eventCore.getPublicEvents();
+        res.status(200).json({events: publicEvents});
+    }
+    catch (error) {
+        res.status(400).json({error: error});
+    }
 };
 
 // /event/:eventID
 
-var getEventAsModerator = function(req, res) {
-    eventCore.getEventByURLAsModerator(req.eventURL)
-    .then((event)=> {
-        // return eventMembershipCore.isUserModerator(req.user, event)
-        return req.getEventMembership()
-        .then((eventMembership)=> {
-            return eventMembership.hasRole("moderator");
-        })
-        .then((result)=> {
-            if(!result) throw Error();
-            return event;
-        });
-    })
-    .then((event)=> {
+var getEventAsModerator = async function(req, res) {
+    try {
+        let event = await eventCore.getEventByURLAsModerator(req.eventURL);
+        let eventMembership = await req.getEventMembership();
+        let isModerator = await eventMembership.hasRole("moderator");
+        if(!isModerator) throw Error();
         res.status(200).json({event: event});
-    })
-    .catch((error)=> {
+    }
+    catch (error) {
         res.status(400).json(error);
-    });
+    }
 };
-var getEventAsRegular = function(req, res) {
-    eventCore.getEventByURL(req.eventURL)
-    .then((event)=> {
-        return returnEventIfVisible(req.user, event);
-    })
-    .then((event)=> {
-        res.status(200).json({event: event});
-    })
-    .catch((error)=> {
+var getEventAsRegular = async function(req, res) {
+    try {
+        let event = await eventCore.getEventByURL(req.eventURL);
+        let visibleEvent = await returnEventIfVisible(req.user, event);
+        res.status(200).json({event: visibleEvent});
+    }
+    catch (error) {
         res.status(400).json(error);
-    });
+    }
 };
 
-module.exports.getEvent = function(req, res) {
+var getEvent = async function(req, res) {
     if(req.header('moderator') == 1) {
         if(!req.user) throw badAuthError;
-        getEventAsModerator(req, res);
+        return await getEventAsModerator(req, res);
     }
     else {
-        getEventAsRegular(req, res);
+        return await getEventAsRegular(req, res);
     }
 };
 
 // /event/:eventID/users/[role]
 
-var getUserList = function(req, res, userListPromise) {
-    return userListPromise
-    .then((userList)=> {
+var getUserList = async function(req, res, userListPromise) {
+    try {
+        let userList = await userListPromise;
         res.status(200).json(userList);
-    })
-    .catch((error)=> {
+    }
+    catch (error) {
         res.status(error.status).json(error.message);
-    });
+    }
 };
 
-module.exports.getAllUsers = function(req, res) {
+var getAllUsers = function(req, res) {
     return getUserList(req, res, EventMembership.getAllMembershipsForEvent(req.event));
-}
-module.exports.getUsersByRole = function(req, res) {
+};
+var getUsersByRole = function(req, res) {
     return getUserList(req, res, EventMembership.getAllMembershipsForEventByRole(req.event, req.params.role));
-}
+};
 
 // /event/:eventID/join
-
-module.exports.joinEvent = function(req, res) {
-    Q.fcall(()=> {
-        var ingress = req.event.ingress;
-        if(ingress=="everyone") {
-            return;
-        }
-        else if(ingress=="link") {
-            assert.equal(req.query.c, event.joinUrl, "Bad link");
-            return;
-        }
-        else if(ingress=="invite") {
-            return req.getEventMembership()
-            .then((eventMembership)=> {
-                if(eventMembership.hasRole("invite")) return event;
-            })
-        }
-    })
-    .then(()=> {
-        return req.getEventMembership()
-        .then((eventMembership)=> {
-            return eventMembership.addRole("attendee");
-        });
-    })
-    .then(()=> {
+var canJoin = async function(req, res) {
+    let ingress = req.event.ingress;
+    if(ingress=="everyone") {
+        return;
+    }
+    else if(ingress=="link") {
+        assert.equal(req.query.c, event.joinUrl, "Bad link");
+        return;
+    }
+    else if(ingress=="invite") {
+        let eventMembership = await req.getEventMembership();
+        if(eventMembership.hasRole("invite")) return;
+    }
+};
+var joinEvent = async function(req, res) {
+    try {
+        await canJoin(req, res);
+        let eventMembership = await req.getEventMembership();
+        await eventMembership.addRole("attendee");
         res.status(200).send();
-    })
-    .fail((error)=> {
-        console.log(error);
-        res.status(400).send();
-    })
-    .done();
+    }
+    catch (error) {
+        res.status(400).json({error: error.message});
+    }
 };
 
-
-var returnEventIfVisible = function(user, event) {
-    return Q.fcall(()=> {
-        if(event.visibility=="public") {
-            return event;
-        }
-        else if(event.visibility=="unlisted") {
-            if(!user) throw badAuthError;
-            return event;
-        }
-        else if(event.visibility=="private") {
-            return EventMembership.getMembership(user, membership)
-            .then((eventMembership)=> {
-                return eventMembership.hasRole("viewer");
-            })
-            .then((result)=> {
-                if(!result) throw badAuthError;
-                return event;
-            })
-            .catch((error)=> {
-                throw badAuthError;
-            });
-        }
-    });
+var returnEventIfVisible = async function(user, event) {
+    if(event.visibility=="public") {
+        return event;
+    }
+    else if(event.visibility=="unlisted") {
+        if(!user) throw badAuthError;
+        return event;
+    }
+    else if(event.visibility=="private") {
+        let eventMembership = await EventMembership.getMembership(user, event);
+        let isViewer = eventMembership.hasRole("viewer");
+        if(!isViewer) throw badAuthError;
+        return event;
+    }
 };
 
-module.exports.moderatorOnly = function(req, res, next) {
-    req.getEventMembership()
-    .then((eventMembership)=> {
-        return eventMembership.hasRole("moderator");
-    })
-    .then((result)=> {
-        if(!result) {
-            return next(badAuthError);
-        }
-        return next();
-    });
+var moderatorOnly = async function(req, res, next) {
+    let eventMembership = await req.getEventMembership();
+    let isModerator = eventMembership.hasRole("moderator");
+    if(!isModerator) {
+        throw badAuthError;
+    }
+    return next();
+};
+
+module.exports = {
+    createEvent: asyncWrap(createEvent),
+    getEvents: asyncWrap(getEvents),
+    getEvent: asyncWrap(getEvent),
+    getAllUsers: asyncWrap(getAllUsers),
+    getUsersByRole: asyncWrap(getUsersByRole),
+    joinEvent: asyncWrap(joinEvent),
+    moderatorOnly: asyncWrap(moderatorOnly)
 };
