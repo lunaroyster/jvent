c9df5c2712af3466cbad8cba1d2f463440385b08
lunaroyster <oneinaquadrillion@live.com>
2017-09-28 07:10:51 +0000

    Migrated more controllers to ES7

--- api/v0/controllers/media.js
+++ api/v0/controllers/media.js
@@ -1,4 +1,3 @@
-const Q = require('q');
 const mediaCore = require('../../../core/media');
 const eventMembershipCore = require('../../../core/eventMembership');
 const assert = require('chai').assert;
@@ -8,46 +7,30 @@ const mediaRequestSchema = require('../requests').media;
 const common = require('./common');
 const validateRequest = common.validateRequest;
 const packError = common.packError;
+const asyncWrap = common.asyncWrap;
 const createMediaTemplateFromRequest = common.createMediaTemplateFromRequest;
 const EventMembership = eventMembershipCore.EventMembership;
 
-var checkCreateMediaPrivilege = function(req) {
-    return Q.fcall(()=> {
-        if(!req.user.privileges.createMedia) throw new Error("Bad privileges");
-        return;
-    })
-    .then(()=> {
-        return req.getEventMembership()
-        .then((eventMembership)=> {
-            assert(eventMembership.hasRole("attendee"), "User is not an attendee"); //TODO: Change role test to privilege test
-            return;
-        })
-    })
+var checkCreateMediaPrivilege = async function(req) {
+    if(!req.user.privileges.createMedia) throw new Error("Bad privileges");
+    let eventMembership = await req.getEventMembership();
+    assert(eventMembership.hasRole("attendee"), "User is not an attendee"); //TODO: Change role test to privilege test
 };
-module.exports.createEventMedia = function(req, res) {
-    Q.fcall(()=> {
-        return validateRequest(req, mediaRequestSchema.createMedia)
-    })
-    .then(()=> {
-        return checkCreateMediaPrivilege(req);
-    })         //Check user privileges
-    .then(()=> {
-        return mediaCore.createEventMedia(createMediaTemplateFromRequest(req));
-    })         //Create media (using authenticated user)
-    .then((media)=> {
+var createEventMedia = async function(req, res) {
+    try {
+        await validateRequest(req, mediaRequestSchema.createMedia);
+        await checkCreateMediaPrivilege(req);
+        let media = await mediaCore.createEventMedia(createMediaTemplateFromRequest(req));
         var state = {
             status: "Created",
-            media: {
-                url: media.url,
-            }
+            media: {url: media.url}
         };
         res.status(201).json(state);
-        return;
-    })    //Send media creation success
-    .catch((error)=> {
+    }
+    catch (error) {
         var err = packError(error);
         res.status(400).json(err);
-    });
+    }
 };
 
 module.exports.getEventMedia = function(req, res) {
@@ -60,4 +43,8 @@ module.exports.getEventMediaByURL = function (req, res) {
 module.exports.appendMediaURL = function(req, res, next) {
     req.mediaURL = req.params.mediaURL;
     next();
+};
+
+module.exports = {
+    createEventMedia: createEventMedia
 };
--- api/v0/controllers/service.js
+++ api/v0/controllers/service.js
@@ -1,35 +1,26 @@
-const Q = require('q');
-
 const imageServiceCore = require('../../../core/imageService');
 
 const serviceRequestSchema = require('../requests').service;
 
 const common = require('./common');
 const validateRequest = common.validateRequest;
+const asyncWrap = common.asyncWrap;
 const packError = common.packError;
 
 
-module.exports.getImageUploadToken = function(req, res) {
-    var checkCreateMediaPrivilege = (req)=> {
-        return Q.fcall(()=> {
-            if(!req.user.privileges.uploadImage) throw new Error("Bad privileges");
-            return;
-        });
-    };
-    Q.fcall(()=> {
-        return validateRequest(req, serviceRequestSchema.getImageUploadToken);
-    })
-    .then(()=> {
-        return checkCreateMediaPrivilege(req);
-    })
-    .then(()=> {
-        return imageServiceCore.generateImageUploadToken(req.query["fileName"], req.query["fileType"]);
-    })
-    .then((uploadToken)=> {
+var getImageUploadToken = async function(req, res) {
+    try {
+        await validateRequest(req, serviceRequestSchema.getImageUploadToken);
+        if(!req.user.privileges.uploadImage) throw new Error("Bad privileges");
+        let uploadToken = await imageServiceCore.generateImageUploadToken(req.query["fileName"], req.query["fileType"]);
         res.status(200).json(uploadToken);
-    })
-    .catch((error)=> {
+    }
+    catch (error) {
         var err = packError(error);
-        res.status(400).json(err);
-    });
+        res.status(400||error.status).json(err);
+    }
+};
+
+module.exports = {
+    getImageUploadToken: asyncWrap(getImageUploadToken)
 };
